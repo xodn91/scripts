b63d2f83d694693825e97f8279387996283a43de
alfonsoh <admin@administrator.local>
2020-06-05 18:19:31 -0700

    adding devops computer setup script

---
+++ bash/devops-setup.sh
@@ -0,0 +1,13 @@
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install Cask
+brew install caskroom/cask/brew-cask
+# Install docker toolbox
+brew cask install docker-toolbox
+#Install Python3
+brew install python3
+# Pull down pip install script
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# Install pip
+python get-pip.py
+
