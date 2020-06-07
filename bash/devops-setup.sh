f47adc5f36f5690689106841318238e75dcf7918
ninjaaung <ninja.aung02@gmail.com>
2020-06-07 14:25:42 -0700

    Added KB and VM

--- bash/devops-setup.sh
+++ bash/devops-setup.sh
@@ -10,4 +10,8 @@ brew install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 # Install pip
 python get-pip.py
+# Install kubernates
+brew install kubectl
+# Cask Install VM
+brew cask install virtualbox
 
