bf6893372219c85a119055023ef49f4dfb20862d
Alfonso Hooker <alfonso.hooker@bitmex.com>
2020-09-03 14:00:03 -0700

    final clean up for wsus deploy

---
+++ docker/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SLEEP=${SLEEP:-"$SLEEP"}
+
+ctrlc()
+{
+    exit 1   # stop the script
+}
+
+trap 'ctrlc' INT
+
+while true; do
+
+  cd /wsus && ./update.sh
+  sleep 10
+
+  chown -R nobody:nogroup /wsus/wsusoffline
+  cd /wsus && ./download.sh 
+  cd /wsus && ./start_web.sh
+
+  echo "Sleeping for $SLEEP before next download."
+  sleep "$SLEEP"
+done
