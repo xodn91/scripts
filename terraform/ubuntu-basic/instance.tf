5f4e0d4147150ffa91e547dddc761df360c7b92b
Sunghee Lee <sung@Sunghees-MacBook-Pro.local>
2020-11-28 14:23:45 -0800

    Change name

--- terraform/ubuntu-basic/instance.tf
+++ terraform/ubuntu-basic/instance.tf
@@ -1,12 +1,12 @@
 resource "aws_key_pair" "tf-ubuntu" {
-  key_name   = "tf-ubuntu"
+  key_name   = "jl-tf-ubuntu"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
 resource "aws_instance" "devops_demo" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.tf-ubuntu.key_name
+  key_name      = aws_key_pair.jl-tf-ubuntu.key_name
 
   provisioner "file" {
     source      = "script.sh"
